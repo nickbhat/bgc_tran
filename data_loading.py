0435adb1d5334c7e39eafbdc89c68aca3f8c6903
Nick Bhattacharya <31296134+nickbhat@users.noreply.github.com>
2019-10-15 20:32:41 -0700

    add comments

--- data_loading.py
+++ data_loading.py
@@ -3,6 +3,11 @@ from typing import Tuple
 import pandas as pd
 
 def load_data_and_counts(path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Loads any of the count tables and returns the full dataframe,
+    as well as a dataframe just containing counts.
+    """
+    
     df = pd.read_csv(path, sep='\t')
     df = df.dropna(axis=1)
     counts = df.select_dtypes(exclude='object') # Drop all columns with strings
@@ -10,6 +15,9 @@ def load_data_and_counts(path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     return df, counts
 
 def load_mibig_metadata(path: str='data/metadata/mibig_metadata.tsv') -> pd.DataFrame:
+    """
+    Loads mibig metadata and incorporates extra annotations.
+    """
     m = pd.read_csv(path, sep='\t')
 
     m.loc[m['Type'] == 'lantipeptide', 'Type'] = 'Lanthipeptide'
