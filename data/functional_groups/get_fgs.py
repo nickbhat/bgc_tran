61da6d727732a1c1e3b5bbc42940cf6114b12b98
alexcritschristoph <acritsc1@jhu.edu>
2020-01-04 17:01:43 -0800

    adding functional group data

---
+++ data/functional_groups/get_fgs.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11b66ab6f60f026686a70d6db164a4a0842daab8181506944c007c81eb28511e
+size 758
---
+++ data/functional_groups/ifg.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71dd96489ef477eb9225dc4b38006bbff78f67d42b82bcae9d0f820756d4ccb1
+size 4145
